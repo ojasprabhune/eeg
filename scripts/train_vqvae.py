a12ef92eab276bfb38bf60820d5799dba265148f
ojasprabhune <prabhuneojas@gmail.com>
2025-11-29 19:52:34 +0000

    wip train vqvae

--- scripts/train_vqvae.py
+++ scripts/train_vqvae.py
@@ -15,30 +15,29 @@ epochs = 10000
 
 vqvae = VQVAE(input_dim=12,
               codebook_size=512,
-              embedding_dim=512)
+              embedding_dim=16)
 vqvae.to(device)
 
-optimizer = optim.AdamW(lr=lr, weight_decay=0.01, params=vqvae.parameters())
+optimizer = optim.AdamW(vqvae.parameters(), lr=lr)
 loss_fn = nn.MSELoss()
-commitment_beta = 1
+commitment_beta = 0.25
 
 appendage_dataset: AppendageDataset = AppendageDataset()
 appendage_dataloader = DataLoader(appendage_dataset, batch_size=32, shuffle=True)
 
 def train():
-    run = wandb.init(
-        name="vq_vae_10_000",
-        entity="prabhuneojas-evergreen-valley-high-school",
-        project="eeg",
-        config={
-            "learning_rate": lr,
-            "architecture": "transformer",
-            "dataset": "region_dataset",
-            "epochs": epochs,
-        },
-    )
+    # run = wandb.init(
+    #     name="vq_vae_10_000",
+    #     entity="prabhuneojas-evergreen-valley-high-school",
+    #     project="eeg",
+    #     config={
+    #         "learning_rate": lr,
+    #         "architecture": "transformer",
+    #         "dataset": "region_dataset",
+    #         "epochs": epochs,
+    #     },
+    # )
 
-    vqvae.to(device)
     iter_tqdm = tqdm(range(epochs))
     for i in tqdm(range(epochs)):
         iter_tqdm.set_description(f"Epoch {i + 1}")
@@ -50,13 +49,19 @@ def train():
             commitment_loss = loss_fn(z_e, z_q.detach())
 
             total_loss = recon_loss + codebook_loss + commitment_beta * commitment_loss
+            print(recon_loss)
+            print(codebook_loss)
+            print(commitment_loss)
 
             iter_tqdm.set_postfix({"loss": total_loss.item()})
-            run.log({"loss": total_loss.item()})
+            # run.log({"loss": total_loss.item()})
 
             optimizer.zero_grad()
             total_loss.backward()
             optimizer.step()
+
+        if i == 5:
+            break
 
         if i % 2000 == 0:
 
@@ -68,7 +73,6 @@ def train():
 
             torch.save(checkpoint, f"/var/log/thavamount/eeg_ckpts/vqvae.pth")
 
-    run.finish()
-    vqvae.to("cpu")
+    # run.finish()
 
 train()
