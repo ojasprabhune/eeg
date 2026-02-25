bf18ffb4013d094058806d86b4ff8fd978341e8f
ojasprabhune <prabhuneojas@gmail.com>
2026-02-25 06:51:46 +0000

    move validation loss to per epoch

--- scripts/train_eeg_cnn.py
+++ scripts/train_eeg_cnn.py
@@ -32,7 +32,8 @@ with open("config/eeg_basic_cnn.yaml", "r") as config_file:
 
 hand_dataset_cnn: HandDatasetCNN = HandDatasetCNN(num_folders=32)
 
-hand_dataloader = DataLoader(hand_dataset_cnn, batch_size=32, shuffle=True)
+hand_dataloader = DataLoader(
+    hand_dataset_cnn, batch_size=batch_size, shuffle=True)
 
 model = EEGCNN(seq_len=hand_dataset_cnn.train_eeg_chunks.shape[-1],
                num_features=num_features,
@@ -47,8 +48,6 @@ optimizer = AdamW(model.parameters(), lr=base_lr, betas=[0.9, 0.98], eps=1e-9)
 loss_fn = CrossEntropyLoss(reduction="none")
 
 model.to(device)
-
-val_chunk_counter = 0
 
 
 def train():
@@ -69,44 +68,29 @@ def train():
     epoch_tqdm = tqdm(range(epochs), dynamic_ncols=True)
     for i in epoch_tqdm:
         epoch_tqdm.set_description(f"Epoch {i + 1}")
-        val_chunk_counter = 0
 
         iter_tqdm = tqdm(hand_dataloader, dynamic_ncols=True)
         for eeg_chunk, label_chunk, mask in iter_tqdm:
             # eeg_chunk: (B, C, T)
             # label_chunk: (B,)
 
-            val_eeg_chunk, val_label_chunk, val_mask_ = hand_dataset_cnn.get_validation_data(
-                val_chunk_counter)
-            val_eeg_chunk = torch.tensor(val_eeg_chunk).to(
-                device).float().unsqueeze(0)  # (1, C, T)
-            val_label_chunk = val_label_chunk.to(device).unsqueeze(0)  # (1,)
-
             eeg_chunk = eeg_chunk.to(device).float()
             label_chunk = label_chunk.to(device)
             mask = mask.to(device)
 
             train_hand_pos_logits = model(eeg_chunk)  # out: (B, vocab_size)
-            val_hand_pos_logits = model(val_eeg_chunk)  # out: (1, vocab_size)
 
             train_loss = loss_fn(train_hand_pos_logits, label_chunk)
-
-            model.eval()
-            val_loss = loss_fn(val_hand_pos_logits, val_label_chunk)
-            model.train()
 
             # mask
             train_loss = (train_loss * mask).sum() / (mask.sum() + 1e-8)
 
             iter_tqdm.set_postfix({"loss": train_loss.item()})
-            run.log({"train loss": train_loss.item(),
-                    "val loss": val_loss.item()})
+            run.log({"train loss": train_loss.item()})
 
             optimizer.zero_grad()  # optimizer has access to all model params, makes grads 0
             train_loss.backward()  # calculates and adds gradients to params so optim sees
             optimizer.step()  # optim looks at gradients and steps accordingly
-
-            val_chunk_counter += 1
 
         if (i + 1) % save_every == 0:
             latest_ckpt = {
@@ -116,6 +100,20 @@ def train():
             }
 
             torch.save(latest_ckpt, f"{save_ckpt_path}_epoch_{i + 1}.pth")
+
+        model.eval()
+        val_eeg_chunk, val_label_chunk, _ = hand_dataset_cnn.get_validation_data(
+            i)
+        val_eeg_chunk = torch.tensor(val_eeg_chunk).to(
+            device).float().unsqueeze(0)  # (1, C, T)
+        val_label_chunk = val_label_chunk.to(device).unsqueeze(0)  # (1,)
+
+        val_hand_pos_logits = model(val_eeg_chunk)  # out: (1, vocab_size)
+
+        val_loss = loss_fn(val_hand_pos_logits, val_label_chunk)
+
+        run.log({"val loss": val_loss.item()})
+        model.train()
 
     run.finish()
 
