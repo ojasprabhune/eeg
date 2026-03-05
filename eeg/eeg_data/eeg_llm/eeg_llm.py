7bae1f195c07b7326a7d249865bd6d561d5a5a10
ojasprabhune <prabhuneojas@gmail.com>
2026-03-05 05:00:42 +0000

    bkp

--- eeg/eeg_data/eeg_llm/eeg_llm.py
+++ eeg/eeg_data/eeg_llm/eeg_llm.py
@@ -1,12 +1,17 @@
 import torch
 import torch.nn as nn
 
+from .transformer import Encoder, Decoder
+
 class EEGLLM(nn.Module):
     """
     Big model to train on EEG and appendate values.
     """
 
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        embedding_dim: int,
+    ) -> None:
         pass
 
     def forward(self):
