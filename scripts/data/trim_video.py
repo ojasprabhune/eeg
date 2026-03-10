5e881cb43d8f7e81bd89e4d2eef1d3f3bca22eb8
ojasprabhune <prabhuneojas@gmail.com>
2026-03-10 03:34:55 +0000

    add trimming ends to trim_video.py

--- scripts/data/trim_video.py
+++ scripts/data/trim_video.py
@@ -39,27 +39,39 @@ def trim_recording(
     raw: mne.io.Raw = mne.io.read_raw_edf(f"{eeg_data_path}.edf", preload=True)
     start_seconds = eeg_trim_start_seconds + start_offset_seconds
     raw_trimmed: mne.io.Raw = raw.copy().crop(tmin=start_seconds)
-    print(f"{Colors.OKBLUE}Trimmed EDF.{Colors.ENDC}")
-    raw_trimmed.save(f"{eeg_data_path}_cut_raw.fif", overwrite=True)
-    print(f"{Colors.OKBLUE}Saved EDF.{Colors.ENDC}\n")
+    print(f"{Colors.OKBLUE}Trimmed EDF.{Colors.ENDC}\n")
 
     # --- hand ---
     print(f"{Colors.OKCYAN}Reading NPY...{Colors.ENDC}")
     hand_data = np.load(f"{hand_data_path}.npy")
     start_frame = int((hand_trim_start_seconds + start_offset_seconds) * fps)
     hand_trimmed = hand_data[:, start_frame:, :]
-    print(f"{Colors.OKCYAN}Trimmed NPY.{Colors.ENDC}")
-    np.save(f"{hand_data_path}_cut.npy", hand_trimmed)
-    print(f"{Colors.OKCYAN}Saved NPY.{Colors.ENDC}\n")
-
-    print(f"EEG data shape: {Colors.BOLD}{raw.get_data().shape}{Colors.ENDC}")
-    print(f"Hand data shape: {Colors.BOLD}{hand_data.shape}{Colors.ENDC}\n")
+    print(f"{Colors.OKCYAN}Trimmed NPY.{Colors.ENDC}\n")
 
     initial_eeg_length = raw.duration
     initial_hand_length = hand_data.shape[1] / fps
 
+    eeg_length = raw_trimmed.duration
+    hand_length = hand_trimmed.shape[1] / fps
+
+    # --- trim to same length ---
+    print(f"{Colors.OKGREEN}Trimming data ends...{Colors.ENDC}\n")
+    min_length = min(eeg_length, hand_length)
+    raw_trimmed.crop(tmin=0, tmax=min_length)
+    n_frames = int(min_length * fps)
+    hand_trimmed = hand_trimmed[:, :n_frames, :]
+
+    raw_trimmed.save(f"{eeg_data_path}_cut_raw.fif", overwrite=True)
+    print(f"{Colors.OKBLUE}Saved EDF.{Colors.ENDC}\n")
+    np.save(f"{hand_data_path}_cut.npy", hand_trimmed)
+    print(f"{Colors.OKCYAN}Saved NPY.{Colors.ENDC}\n")
+
+    # --- stats ---
     final_eeg_length = raw_trimmed.duration
     final_hand_length = hand_trimmed.shape[1] / fps
+
+    print(f"EEG data shape: {Colors.BOLD}{raw.get_data().shape}{Colors.ENDC}")
+    print(f"Hand data shape: {Colors.BOLD}{hand_data.shape}{Colors.ENDC}\n")
 
     print(f"{Colors.OKCYAN}EEG data length before: {round(initial_eeg_length, 1)} seconds.{Colors.ENDC}")
     print(f"{Colors.OKCYAN}Hand data length before: {round(initial_hand_length, 1)} seconds.{Colors.ENDC}\n")
