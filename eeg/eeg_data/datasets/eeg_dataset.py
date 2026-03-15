more masking & fix duration logits

eeg/eeg_data/datasets/eeg_dataset.py
@@ -183,13 +183,13 @@ class EEGDataset(Dataset):
             next_tok = tokens[i + 1]
 
             if next_tok == current_tok:
-                masks.append(1e-3)
+                masks.append(1e-4)
             else:
                 masks.append(1)
 
         return torch.tensor(eeg), torch.tensor(apps), torch.tensor(tokens), torch.tensor(durations), torch.tensor(masks)
 
-    def get_val_data(self, index: int) -> tuple[list[list[int]], list[list[int]], list[int]]:
+    def get_val_data(self, index: int) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         """
         Returns the EEG data, appendage data, and VQ-VAE tokens for the given
         index from the validation set.
@@ -199,4 +199,4 @@ class EEGDataset(Dataset):
         apps: list[list[int]] = self.val_app_chunks[index]
         tokens: list[int] = self.val_token_chunks[index]
 
-        return eeg, apps, tokens
+        return torch.tensor(eeg), torch.tensor(apps), torch.tensor(tokens)

eeg/eeg_data/eeg_llm/eeg_llm.py
@@ -82,6 +82,6 @@ class EEGLLM(nn.Module):
         durations = self.relu(durations)
         durations = self.duration_prediction_linear3(durations)
         durations = self.relu(durations)
-        durations = self.duration_prediction_linear4(durations).unsqueeze(-1)
+        durations = self.duration_prediction_linear4(durations).squeeze(-1)
 
         return vocab_distribution, durations

scripts/train_big_eeg_llm.py
@@ -90,8 +90,8 @@ def train():
             eeg = eeg.to(device).float()
             apps = apps.to(device).float()
             tokens = tokens.to(device)
-            durations = torch.tensor(durations).to(device)
-            masks = torch.tensor(masks).to(device)
+            durations = durations.to(device)
+            masks = masks.to(device)
 
             token_logits, durations_logits = model(eeg) # out: (B, T, vocab_size), (B, T)
             token_logits = token_logits.transpose(1, 2) # (B, T, vocab_size) -> (B, vocab_size, T)
@@ -111,6 +111,7 @@ def train():
             optimizer.zero_grad()  # optimizer has access to all model params, makes grads 0
             total_loss.backward()  # calculates and adds gradients to params so optim sees
             optimizer.step()  # optim looks at gradients and steps accordingly
+
 
         if (i + 1) % save_every == 0:
             latest_ckpt = {